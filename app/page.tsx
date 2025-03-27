12af087e4029e9769da545d953c8085063c8b06a
em-ess <michssong@gmail.com>
2025-03-26 23:35:09 -0500

    daily commit lol

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main>
         <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-          saveur
+          saveur (hbd conrad!)
         </code>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
